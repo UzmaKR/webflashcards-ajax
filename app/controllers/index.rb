6acf124eae07630d6b1c15255ebb109f354a823d
UzmaKR <uzma.rahman@gmail.com>
2013-06-07 16:38:50 -0700

    started UI merge of rounds & users

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,7 +1,7 @@
 enable :sessions
 
 get '/' do
-  session.clear if session[:id]
+  session.clear if session[:round_id]
   # Look in app/views/index.erb
   erb :index
 end
@@ -17,18 +17,18 @@ post '/play' do
     @message = "You suck at this game! The answer is obviously #{answer}."
   end
 
-  @card = Deck.find(session[:id]).cards.sample
+  @card = Deck.find(session[:round_id]).cards.sample
 
   erb :game
 end
 
 get '/deck/:id' do
   
-  session[:id] = params[:id]
+  session[:round_id] = params[:id]
   session[:num_correct] = 0
   # session[:num_guessed] = 0
   session[:num_incorrect] = 0
-  @card = Deck.find(session[:id]).cards.sample
+  @card = Deck.find(session[:round_id]).cards.sample
   erb :game
 end
 
@@ -36,10 +36,10 @@ get '/results' do
 
   @num_incorrect = session[:num_incorrect]
   @num_correct = session[:num_correct]
-  @deck_topic = Deck.find(session[:id]).topic
+  @deck_topic = Deck.find(session[:round_id]).topic
 
   Round.create(num_correct: @num_correct, num_incorrect: @num_incorrect,
-               deck_id: session[:id])
+               deck_id: session[:round_id])
   session.clear
   erb :results
 end
