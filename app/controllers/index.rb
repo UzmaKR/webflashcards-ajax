249c4f2c33186e5a9e20fe6ec3d33cd08062e264
dbc-apprentice <dbc-apprentice@devbootcamp.com>
2013-06-07 12:22:02 -0700

    Change view to show messages

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -5,7 +5,23 @@ end
 
 get '/play' do
   @card = Card.random
+  @question = @card.front
+  @answer = @card.back
 
+  erb :game
+end
 
+post '/play' do
+  guess = params[:guess]
+  answer = params[:answer]
+  if guess == answer
+    @message = "Correct!"
+  else
+    @message = "You suck at this game! The answer is obviously #{answer}."
+  end
+  @card = Card.random
+  @question = @card.front
+  @answer = @card.back
+  
   erb :game
 end
