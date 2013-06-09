16dacc04881da962ad2530bb08f5983ebc508c1d
Apprentice <apprentice@dbc03.local>
2013-06-08 17:03:22 -0700

    ajax works

--- public/js/application.js
+++ public/js/application.js
@@ -4,4 +4,22 @@ $(document).ready(function() {
   // when we try to bind to them
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $("form").on('click','#guessbutton',function(e){
+    console.log(e);
+    e.preventDefault();
+    $.ajax({
+        type: 'POST',
+        url: '/play',
+        data: $("form").serialize()
+    }).done(function(response) {
+        // console.log(response.message);
+        // console.log(response.front);
+        // console.log(response.back);
+        $('#server_answer').text(response.message);
+        window.setTimeout(function() {
+          $('#card_answer').attr('value', response.back);
+          $('#new_front').text(response.front);
+        }, 3000);
+      });
+  });
 });
